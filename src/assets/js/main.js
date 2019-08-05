734ce35f277de3b279790e991d65241f3d037fcc
artiscodedev <artiscodedev@gmail.com>
2019-08-05 15:38:21 -0700

    base
    base

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,17 @@
-$(document).ready(function () {
-    let demo = new demoObj(20,5);
-    demo.jqbuild();
-});
+(function($) {
+  "use strict";
+  $(window).on({
+    load: function() {
+      console.log("preloader");
+
+      let preloader = new ctmPreloader();
+
+      // preloader.init();
+      console.log("preloader end");
+    }
+  });
+
+  $(document).ready(function() {
+    //do something
+  });
+})(jQuery);
