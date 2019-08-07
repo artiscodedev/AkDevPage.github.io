67103f018381cddfc2e9be40d1928a609b923242
artiscodedev <artiscodedev@gmail.com>
2019-08-06 23:18:31 -0700

    update

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,10 +8,38 @@
 
       // preloader.init();
       console.log("preloader end");
+    },
+    scroll: function() {
+      // FIX HEADER ON SCROLL
+      var e = $(window).scrollTop();
+      $(window).height();
+      e > 1
+        ? $(".header").addClass("header-fixed")
+        : $(".header").removeClass("header-fixed");
+
+      // BACK TO TOP
+      if ($(this).scrollTop() > 100) {
+        $("#back-top").fadeIn();
+      } else {
+        $("#back-top").fadeOut();
+      }
     }
   });
 
   $(document).ready(function() {
     //do something
+    let empresas = new compEmpresas();
+
+    $("#back-top a").on("click", function() {
+      $("body,html")
+        .stop(false, false)
+        .animate(
+          {
+            scrollTop: 0
+          },
+          800
+        );
+      return false;
+    });
   });
 })(jQuery);
